c3e34d9a6640fb78bd66f0c9a64253c867f8d8c5
Tishlyy <ekaetteessien27@gmail.com>
2022-05-31 23:31:22 +0100

    Decimal to hexadecimal

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    numList = f"{i:02d}"
-    print("{}".format(numList), end=", ")
+    if i == 99:
+        print("{}".format(i))
+    else:
+        print("{:02d}".format(i), end=", ")
