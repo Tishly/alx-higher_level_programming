462e87414d7d7dbc32e80b3c44283099c09d46d8
Tishlyy <ekaetteessien27@gmail.com>
2022-06-01 00:51:42 +0100

    Print last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
+
 def print_last_digit(number):
-    
+
     number = str(number)
     last_digit = int(number[-1])
     number = int(number)
     if number < 0:
-        last_digit = -last_digit
+        last_digit = last_digit
     else:
         last_digit = abs(last_digit)
     print("{}".format(last_digit), end="")
