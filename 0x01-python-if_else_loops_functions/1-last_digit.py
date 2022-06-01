fd1fe0d5fc0be99e56da8aa54ce1ea28022380a1
Tishlyy <ekaetteessien27@gmail.com>
2022-06-01 02:09:10 +0100

    Lasst digit redo

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,7 +8,7 @@ if number < 0:
     last_digit = -last_digit
 else:
     last_digit = abs(last_digit)
-msg = f"Last digit of {number} is {last_digit} "
+msg = "Last digit of {} is {} ".format(number, last_digit)
 if last_digit > 5:
     print(msg + "and is greater than 5")
 elif last_digit == 0:
