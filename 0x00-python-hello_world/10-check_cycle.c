a451979a3b411ed433c046460a1958c5f8e5084b
Tishlyy <ekaetteessien27@gmail.com>
2022-06-01 21:01:35 +0100

    Last digit

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+#include <stlib.h>
+
+/**
+ * check_cycle - function that checks if a
+ * linked list has a cycle in it
+ *
+ * list - list parameters
+ *
+ * Return: 1 if there is no cycle, 0 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t 
+}
