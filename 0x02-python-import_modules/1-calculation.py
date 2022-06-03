6e101a1cb860c16c9a595ded7029a3c040d01e37
Tishlyy <ekaetteessien27@gmail.com>
2022-06-03 10:18:29 +0100

    multipleFunctions

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,4 +1,6 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
+
+
 if __name__ == '__main__':
 
     from calculator_1 import add, sub, mul, div
@@ -6,6 +8,6 @@ if __name__ == '__main__':
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} + {} = {}".format(a, b, sub(a, b)))
-    print("{} + {} = {}".format(a, b, mul(a, b)))
-    print("{} + {} = {}".format(a, b, div(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
