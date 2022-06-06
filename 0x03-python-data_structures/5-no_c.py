27d28e033917294a069e0f611015b80569e0ff9c
Tishlyy <ekaetteessien27@gmail.com>
2022-06-06 14:39:22 +0100

    Remove c and C

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,4 +2,8 @@
 
 
 def no_c(my_string):
-
+    if my_string[:]:
+        new_string = my_string.translate({ord('c'): None})
+        new_string2 = new_string.translate({ord('C'): None})
+        return new_string2
+    return my_string
