951c398c0bb2a68bd91920387713b5b4c240a9f3
Tishlyy <ekaetteessien27@gmail.com>
2022-06-07 00:54:46 +0100

    Switch

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,13 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        if not count % 3:
-            print("{}".format(count))
-        print("{}".format(item))
-        count += 1
+    rows = len(matrix)
+    columns = len(matrix[0])
+
+    matrix_T = []
+    for j in range(columns):
+        row = []
+        for i in range(rows):
+            row.append(matrix[i][j])
+        matrix_T.append(row)
+    print("{}".format(matrix_T))
