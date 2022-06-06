bc25b5098b7737fee2f656ffc8e17dcb693fd5e0
Tishlyy <ekaetteessien27@gmail.com>
2022-06-06 19:19:50 +0100

    Tuple

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    x = '\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in matrix])
-    print("{}".format(x))
+    for index, item in enumerate(matrix, start=1):
+        print(item)
+        if not index % 3:
+            print(index)
