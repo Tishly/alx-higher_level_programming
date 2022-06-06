e84294fbd69365965d65bc5a98e7191e5b985c6c
Tishlyy <ekaetteessien27@gmail.com>
2022-06-07 00:43:05 +0100

    Delete at point

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,9 +2,6 @@
 
 
 def delete_at(my_list=[], idx=0):
-    new_list = my_list[:]
-    if idx in range(len(new_list)):
-        i = new_list[idx]
-        new_list.remove(i)
-        return new_list
-    return my_list
+    if idx in range(len(my_list)):
+        del my_list[idx]
+        return my_list
