8a0759d47644246a5a35ec232885d24fa570083c
Tishlyy <ekaetteessien27@gmail.com>
2022-06-09 00:28:26 +0100

    Argument count

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    from sys import argv
+    x = len(argv) - 1
+    if x == 0:
+        print("0 arguments.")
+    if x == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(x))
+
+    for i in range(x):
+        print("{}: {}".format(i + 1, argv[i + 1]))        
