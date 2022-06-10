26030f0d029b7e780c6278efdb84c61b2965542b
Tishlyy <ekaetteessien27@gmail.com>
2022-06-10 20:18:29 +0100

    List addition without duplicating elements

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+# add a list of integers in a list
+# only once per integer
+def uniq_add(my_list=[]):
+    new_list = []
+    new_list = [new_list.append(x) for x in my_list if x not in new_list]
+    summ = 0
+    for i in range(len(new_list) + 1):
+        summ += i
+    return summ
