e45146c6e5e0a4abc2f2e9f2f05f8ffbddf2928b
Tishlyy <ekaetteessien27@gmail.com>
2022-06-11 00:30:45 +0100

    Sorted a dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 
 
 def print_sorted_dictionary(a_dictionary):
-    [print("{:s}: {}".format(key, a_dictionary[key])) for key in sorted(a_dictionary)]
+    [print("{:s}: {}".format(k, a_dictionary[k])) for k in sorted(a_dictionary)]
