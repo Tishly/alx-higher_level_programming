9d72ca8c218cd1d9fc7fce223ce4a030bc63a091
Tishlyy <ekaetteessien27@gmail.com>
2022-06-21 01:20:44 +0100

    Print division with Finally

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,7 +5,7 @@ def safe_print_division(a, b):
     result = 0
     try:
         result = a / b
-    except ZeroDivisionError:
+    except (TypeError, ZeroDivisionError, ValueError):
         return None
     finally:
         print("Inside result: {}".format(result if result else None))
