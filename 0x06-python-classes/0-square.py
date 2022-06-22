ed26d39715496b6fd3b0258d7fa2cf1367974919
Tishlyy <ekaetteessien27@gmail.com>
2022-06-22 21:04:55 +0100

    Square documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
+"""An empty class Square that defines a square"""
+
 
 class Square:
-    '''An empty class Square that defines a square'''
+    """A square"""
     pass
-    '''An empty block of code that does nothing'''
