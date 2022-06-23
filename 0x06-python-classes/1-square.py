1512b2d2976d8df63fc3396eb85de875e10ec305
Tishlyy <ekaetteessien27@gmail.com>
2022-06-23 15:56:02 +0100

    Square instant

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Square"""
+
+
+class Square:
+    def __init__(self, size):
+        """Private instance attribute: size. Instantiation with size"""
+        self.__size = size
