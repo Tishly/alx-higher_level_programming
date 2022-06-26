ffd105909cf3093cead9414cbae4868f0945bdc1
Tishlyy <ekaetteessien27@gmail.com>
2022-06-26 18:38:18 +0100

    Get value of private attribute size

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""Define a square"""
+
+
+class Square:
+    """Represent a new square"""
+    def __init__(self, size=0):
+        """Initialize a new square.
+        Args:
+        size (int): instance attribute"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        return (value)
+
+    def area(self):
+        """Return the square area"""
+        return (self.__size * self.__size)
