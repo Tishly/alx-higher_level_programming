f64c5bcc542f71737dddad69182744c1ce74df46
Tishlyy <ekaetteessien27@gmail.com>
2022-06-27 20:53:48 +0100

    Height of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -20,13 +20,11 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """setter for private instance attribute width"""
-        try:
-            if isinstance(value, int) and value >= 0:
-                self.__width = value
-        except TypeError:
-            raise ("width nmust be an integer")
-        except ValueError:
-            raise ("width must be >= 0")
+        if type(value) is not int:
+            raise TypeError("width nmust be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
@@ -36,10 +34,8 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """setter for private instance attribute height"""
-        try:
-            if isinstance(value, int) and value >= 0:
-                self.__height = value
-        except TypeError:
-            raise ("height nmust be an integer")
-        except ValueError:
-            raise ("height must be >= 0")
+        if type(value) is not int:
+            raise TypeError("height nmust be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
