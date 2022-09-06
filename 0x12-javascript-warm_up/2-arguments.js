55df446c65a7d44c5d3760bf108aa7bf497030be
Tishlyy <ekaetteessien27@gmail.com>
2022-09-06 05:39:41 +0100

    Corrected code

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,15 @@
 #!/usr/bin/node
 
-let myVar = process.argv.forEach((index) => {
-  myVar = index + 1;
-});
+/*
+ * script that prints a message depending
+ *on the number of arguments passed:
+ */
+
+const myVar = process.argv.length - 2;
 
 if (myVar === 0) {
   console.log('No argument');
-}
-if (myVar === 1) {
+} else if (myVar === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
