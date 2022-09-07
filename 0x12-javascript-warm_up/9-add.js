88db6d844c8bf1057d75fcc2a822e9941a7b58a0
Tishlyy <ekaetteessien27@gmail.com>
2022-09-07 06:14:53 +0100

    Addition

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+/*
+ * A scripts that prints the addition of two integers
+ */
+
+const num1 = process.argv[2];
+const num2 = process.argv[3];
+
+function add (a, b) {
+  if (isNaN(a) || isNaN(b)) {
+    return ('NaN');
+  } else {
+    return (parseInt(a) + parseInt(b));
+  }
+}
+console.log(add(num1, num2));
