8a22e2a342dca629ea3af595d2f775cb09dc1f5c
Tishlyy <ekaetteessien27@gmail.com>
2022-09-09 15:35:10 +0100

    Rectangle double and rotate

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+/*
+ * A class Rectangle that defines a rectangle
+ */
+
+class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const row = new Array(this.width).fill('X', 0, this.width);
+    const rows = new Array(this.height).fill(row.join(''), 0, this.height);
+    console.log(rows.join('\n'));
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+module.exports = Rectangle;
