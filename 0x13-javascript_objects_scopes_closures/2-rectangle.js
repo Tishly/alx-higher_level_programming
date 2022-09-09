8f1c6f78e9ae161d2c5ab065ea828531e29979fd
Tishlyy <ekaetteessien27@gmail.com>
2022-09-09 10:02:07 +0100

    Manage return if 0 or neg

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+/*
+ * A class Rectangle that defines a rectangle
+ */
+
+class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
