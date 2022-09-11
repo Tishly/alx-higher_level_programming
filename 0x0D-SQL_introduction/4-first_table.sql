c8b1e58ca14862b43e3962d2160b69d323c696c3
Tishlyy <ekaetteessien27@gmail.com>
2022-09-11 20:43:36 +0100

    Code fix

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
--- A script that creates a table called first_table in the current datbases in your MySQL server
+-- A script that creates a table called first_table in the current databases in your MySQL server
 
 CREATE TABLE first_table (
 	id INT, name VARCHAR(256));
