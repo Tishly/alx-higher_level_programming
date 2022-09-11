d3d502eb3919aa4455e46cd4603b2f5982737a72
Tishlyy <ekaetteessien27@gmail.com>
2022-09-11 07:03:57 +0100

    Map

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const list = require('./100-data.js').list;
+/*
+ * A script that imports an array and computes a new array
+ * A new list must be created with each value equal to the
+ *value of the initial list, multipled by the index in the list
+ * Print both the initial list and the new list
+ */
+
+let index = 0;
+let newArray = [];
+for (const elem in list) {
+  newArray = list.map((elem, index) => elem * index);
+  ++index;
+}
+console.log(list);
+console.log(newArray);
