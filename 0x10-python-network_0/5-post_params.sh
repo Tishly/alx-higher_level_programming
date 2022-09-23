8428d08fa49e8e071d301799a76d61f55155ca7d
Tishly <ekaetteessien27@gmail.com>
2022-09-22 21:56:57 -0700

    Post params

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script to set email and subject header variables
-curl -s -X POST -d "email: test@gmail.com&subject: I will always be here for PLD" "$1"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
