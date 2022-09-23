41cf2c655e130af38a9ab6d415c5834906b99052
Tishly <ekaetteessien27@gmail.com>
2022-09-22 20:02:16 -0700

    Body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that takes in and sends a request to a URL, then displays the content-length
-curl -sI "{$url}" | grep -i content-lenght
+curl -sI '$1' | grep -iE 'Content-Lenght: [0-9]+' | cut -d ' ' -f2
