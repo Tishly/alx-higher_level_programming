7650f4ed7220a67c0016c48d2ab6be735ac4a402
Tishly <ekaetteessien27@gmail.com>
2022-09-26 08:07:47 -0700

    Email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,4 +10,11 @@ import sys
 
 if __name__ == '__main__':
     url = sys.argv[1]
-    respone = 
+    e_address =  sys.argv[2]
+    values = {'email': 'e_address'}
+   """ data = urllib.parse.urlencode(values)
+    data = data.encode('utf8')"""
+    req = urllib.request.Request(url, data)
+    with urlopen(req) as response:
+        headers = response.info()
+        print('Your email is: {}'.format(response.headers['email']))
