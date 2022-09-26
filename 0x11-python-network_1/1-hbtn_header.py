8ca401bb42c986dc7f74096c8ded51e4d374a710
Tishly <ekaetteessien27@gmail.com>
2022-09-26 06:06:43 -0700

    Applied requirement

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,6 +8,6 @@ import sys
 
 if __name__ == '__main__':
     url = sys.argv[1]
-    response = urlopen(url)
-    headers = response.info()
-    print(response.headers['X-Request-Id'])
+    with urlopen(url) as response:
+        headers = response.info()
+        print(response.headers['X-Request-Id'])
