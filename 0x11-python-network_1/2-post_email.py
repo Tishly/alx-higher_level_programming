826f4321741aa3e12cdbe4b6b7892d92f18655b6
Tishly <ekaetteessien27@gmail.com>
2022-09-26 08:15:09 -0700

    email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,11 +8,12 @@ from urllib.request import urlopen as lop
 from urllib.parse import urlencode as enc
 import sys
 
-url = sys.argv[1]
-address = sys.argv[2]
-values = {"email": address}
-data = enc(values)
-data = data.encode('utf8')
-req =  urllib.request.Request(url, data)
-with lop(req) as response:
-    print(response.read().decode('utf8'))
+if __name__ == '__main__':
+    url = sys.argv[1]
+    address = sys.argv[2]
+    values = {"email": address}
+    data = enc(values)
+    data = data.encode('utf8')
+    req =  urllib.request.Request(url, data)
+    with lop(req) as response:
+        print(response.read().decode('utf8'))
