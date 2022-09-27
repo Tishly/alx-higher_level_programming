f928e8393c91a35801af07c31b0b385dd2d38b4e
Tishly <ekaetteessien27@gmail.com>
2022-09-27 04:51:17 -0700

    Request error handling

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,13 +4,12 @@ Script that If the HTTP status code is greater than or equal to 400,
 print: Error code: followed by the value of the HTTP status code
 """
 
-from requests.exceptions import HTTPError
+
 import requests as req
 import sys
 
-url = sys.argv[1]
-
 if __name__ == '__main__':
+    url = sys.argv[1]
     res = req.get(url)
     if res.status_code == 200:
         print(res.text)
