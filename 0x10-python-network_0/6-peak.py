8fb7d159c2f7009c911d15255106b631fee46a91
Tishlyy <ekaetteessien28@gmail.com>
2022-09-27 20:21:46 -0700

    Peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,4 +4,9 @@ A function hat finds a peakin a list of unsorted integers
 """
 
 def find_peak(list_of_integers):
-
+    """
+    finds peak integer
+    """
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
