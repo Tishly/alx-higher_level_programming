3ce41d46d9e44121f372117c133c5acef4a5bae1
Tishlyy <ekaetteessien28@gmail.com>
2022-10-20 03:52:03 -0700

    Created Position  private instance and set test cases

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -18,23 +18,22 @@ class Square:
     @property
     def size(self):
         """Gets the value of private attribute size"""
-        return self.__size
+        return (self.__size)
 
     @size.setter
     def size(self, value):
         """Changes value of private attribute size
         Args:
             value (int): new value of size"""
-        self.__size = value
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        return (value)
+        self.__size = value
 
         @property
         def position(self):
-            return self.__position
+            return (self.__position)
 
         @position.setter
         def position(self, value):
@@ -52,11 +51,11 @@ class Square:
     def my_print(self):
         """Print a square with '#' characters"""
         if self.__size == 0:
-            print()
+            print("")
             return
 
         [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(self.__size)]
-            print()
+            print("")
