39db38ceebd2bd81dd9eb11b56d4761b7bd9c9d2
Tishly <ekaetteessien27@gmail.com>
2022-11-09 14:20:03 +0000

    Checking inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,7 +6,7 @@ def inherits_from(obj, a_class):
     A function that checks if the object is a child class of an instance
     """
 
-    if issubclass(obj, a_class):
+    if isinstance(obj, a_class):
         return True
     else:
         return False
