2ab9f64f89cfb93a48c2df61bb2fda941c5d6c13
Tishly <ekaetteessien27@gmail.com>
2022-11-09 12:59:56 +0000

    Checking if object is a kind of class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def inherits_from(obj, a_class):
+    """
+    A function that checks if the object is a child class of an instance
+    """
+
+    if issubclass(obj, a_class):
+        return True
+    else:
+        return False
