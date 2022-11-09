a4775c611be79b3031382a2a516f940a1facd5dd
Tishly <ekaetteessien27@gmail.com>
2022-11-09 13:57:42 +0000

    fixed type

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -5,6 +5,7 @@ A function that returns True if the object is
 exactly an instance of the specified class ; otherwise False
 """
 
+
 def is_kind_of_class(obj, a_class):
     """
     A function that returns True if the object is
